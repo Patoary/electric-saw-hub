d9ab8b299bf5db13c52282a77ffc9b6404a8f3af
patoary <zahidsagar93@gmail.com>
2022-09-03 22:48:57 +0900

    products load in home page

---
+++ src/Pages/Products/ProductCard.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const ProductCard = ({ saw }) => {
+    const { name, price, img, description, available, minOrderQuantity } = saw;
+    return (
+        <section>
+            <div className='card card-compact max-w-lg bg-base-100 shadow-xl mt-10 mx-auto duration-200'>
+                    <figure
+                    className='h-56 w-40 flex justify-center items-center mx-auto p-5'
+                    ><img src={img} alt="saw" />
+                    </figure>
+                    <div class="card-body">
+                        <h2 class="card-title text-primary mx-auto text-3xl">{name}</h2>
+                        <p>{description.slice(0, 110)}...</p>
+                        <div className='flex justify-between'>
+                            <h4 className='text-md font-semibold text-[#19d3aede]'>Available : {available}</h4>
+                            <h4 className='text-md font-semibold text-[#19d3aede]'>Minimum Ordre : {minOrderQuantity}</h4>
+                        </div>
+
+                        <div className='flex justify-between'>
+                            <h3 className='text-secondary font-bold text-2xl lg:4xl'>Price : ${price}</h3>
+                            <button class="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+         
+        </section>
+    );
+};
+
+export default ProductCard;
--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -2,17 +2,30 @@ import React from 'react';
 import { useQuery } from 'react-query';
 import Heading from '../../Components/Heading/Heading';
 import './Products.css';
+import Loding from '../../Components/Loading/Loading';
+import ProductCard from './ProductCard';
 const Products = () => {
-    const {data:saws} = useQuery('all-saws', () =>{
+    const { data: saws, isLoading } = useQuery('all-saws', () =>
         fetch('saws.json')
-        .then(res => res.json())
-
-    })
+            .then(res => res.json())
+    )
+    if (isLoading) {
+        return <Loding />
+    }
     return (
-        <div className='bg-gradient-to-r from-[#00214124] to-[#19d3ae2e] py-10'>
-            <div><Heading>Our Manufacturing Line</Heading></div>
+        <div className=''>
+            <div className='bg-gradient-to-r from-[#00214124] to-[#19d3ae2e] py-10 px-12'>
+                <div><Heading>Our Manufacturing Line</Heading></div>
+                <div className='grid grid-cols-1 lg:grid-cols-3 gap-10'>
+                    {
+                        [...saws]?.reverse().slice(0, 3).map(saw => <ProductCard
+                            key={saw._id}
+                            saw={saw}
+                        ></ProductCard>)
+                    }
+                </div>
 
-            
+            </div>
         </div>
     );
 };
